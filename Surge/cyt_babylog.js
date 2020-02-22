16aa55be6f279dcea66db87bf157ab82691f546d
Goope <Goope.Liu@gmail.com>
2020-02-22 17:59:08 +0800

    cyt babylog

--- Surge/cyt_babylog.js
+++ Surge/cyt_babylog.js
@@ -1,26 +1,20 @@
-/*
-README：https://github.com/yichahucha/surge/tree/master
- */
-
-const path1 = "serverConfig";
-const path2 = "wareBusiness";
 const consolelog = false;
-const url = $request.url;
-const body = $response.body;
-const $tool = tool();
+const forward_url = "http://192.168.50.105:9090/cyt"
 
-const $forward_url = "http://192.168.50.105:9090/cyt"
+forward_body();
+$done({body: $response.body});
 
 function forward_body() {
     const options = {
-        url: $forward_url,
+        url: forward_url,
         // headers: {
         //     "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
         //     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 - mmbWebBrowse - ios"
         // },
-        body: $body,
+        body: $response.body,
     }
-    $tool.post(options, function (error, response, data) {
+    $httpClient.post(options, function (error, response, data) {
+        // console.log(data);
         if (!error) {
             if (consolelog) console.log("Data:\n" + data);
         } else {
@@ -28,6 +22,3 @@ function forward_body() {
         }
     })
 }
-
-forward_body();
-$done({body: body});
