31188c7404065d1457d3fe7e12c5718371a358bc
Goope <Goope.Liu@gmail.com>
2020-03-02 09:52:34 +0800

    update js

--- Surge/cyt_babylog.js
+++ Surge/cyt_babylog.js
@@ -1,23 +1,26 @@
-const consolelog = false;
-const forward_url = "http://192.168.50.105:9090/cyt"
+const consolelog = true;
+const forward_url = "http://192.168.50.50:19091/cyt"
+// const forward_url = "http://192.168.50.105:9091/cyt"
 
-forward_body();
-$done({body: $response.body});
+var ssidMatched = ($network.wifi.ssid === 'Tomato50');
+
+if (ssidMatched) {
+    forward_body();
+}
+
+$done({ body: $response.body });
 
 function forward_body() {
     const options = {
         url: forward_url,
-        // headers: {
-        //     "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        //     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 - mmbWebBrowse - ios"
-        // },
         body: $response.body,
     }
     $httpClient.post(options, function (error, response, data) {
-        // console.log(data);
         if (!error) {
+            $notification.post("育学园数据同步成功！", "", "");
             if (consolelog) console.log("Data:\n" + data);
         } else {
+            $notification.post("育学园数据同步失败！", "", "");
             if (consolelog) console.log("Error:\n" + error);
         }
     })
