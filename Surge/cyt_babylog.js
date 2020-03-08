cffd74227fc328803db16ecf94a11fce6ff22b56
Goope <Goope.Liu@gmail.com>
2020-03-08 08:49:51 +0800

    babylog surpport quanx

--- Surge/cyt_babylog.js
+++ Surge/cyt_babylog.js
@@ -1,13 +1,14 @@
 const consolelog = true;
 const forward_url = "http://192.168.50.50:19091/cyt"
-// const forward_url = "http://192.168.50.105:9091/cyt"
+const $tool = tool();
 
-var ssidMatched = ($network.wifi.ssid === 'Tomato50');
+// var ssidMatched = ($network.wifi.ssid === 'Tomato50');
 
-if (ssidMatched) {
-    forward_body();
-}
+// if (ssidMatched) {
+//     forward_body();
+// }
 
+forward_body();
 $done({ body: $response.body });
 
 function forward_body() {
@@ -15,13 +16,87 @@ function forward_body() {
         url: forward_url,
         body: $response.body,
     }
-    $httpClient.post(options, function (error, response, data) {
+    $tool.post(options, function (error, response, data) {
         if (!error) {
-            $notification.post("育学园数据同步成功！", "", "");
+            $tool.notify("育学园数据同步成功！", "", "");
             if (consolelog) console.log("Data:\n" + data);
         } else {
-            $notification.post("育学园数据同步失败！", "", "");
+            $tool.notify("育学园数据同步失败！", "", "");
             if (consolelog) console.log("Error:\n" + error);
         }
     })
+}
+
+function tool() {
+    const isSurge = typeof $httpClient != "undefined"
+    const isQuanX = typeof $task != "undefined"
+    const isResponse = typeof $response != "undefined"
+    const node = (() => {
+        if (typeof require == "function") {
+            const request = require('request')
+            return ({ request })
+        } else {
+            return (null)
+        }
+    })()
+    const notify = (title, subtitle, message) => {
+        if (isQuanX) $notify(title, subtitle, message)
+        if (isSurge) $notification.post(title, subtitle, message)
+        if (node) console.log(JSON.stringify({ title, subtitle, message }));
+    }
+    const write = (value, key) => {
+        if (isQuanX) return $prefs.setValueForKey(value, key)
+        if (isSurge) return $persistentStore.write(value, key)
+    }
+    const read = (key) => {
+        if (isQuanX) return $prefs.valueForKey(key)
+        if (isSurge) return $persistentStore.read(key)
+    }
+    const adapterStatus = (response) => {
+        if (response) {
+            if (response.status) {
+                response["statusCode"] = response.status
+            } else if (response.statusCode) {
+                response["status"] = response.statusCode
+            }
+        }
+        return response
+    }
+    const get = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "GET"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) $httpClient.get(options, (error, response, body) => {
+            callback(error, adapterStatus(response), body)
+        })
+        if (node) {
+            node.request(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    const post = (options, callback) => {
+        if (isQuanX) {
+            if (typeof options == "string") options = { url: options }
+            options["method"] = "POST"
+            $task.fetch(options).then(response => {
+                callback(null, adapterStatus(response), response.body)
+            }, reason => callback(reason.error, null, null))
+        }
+        if (isSurge) {
+            $httpClient.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+        if (node) {
+            node.request.post(options, (error, response, body) => {
+                callback(error, adapterStatus(response), body)
+            })
+        }
+    }
+    return { isQuanX, isSurge, isResponse, notify, write, read, get, post }
 }
