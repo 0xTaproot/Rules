9f6d76cb6aa015a0b674682495ad8c4464d61f1f
Richard <6115161+0xTaproot@users.noreply.github.com>
2022-11-11 10:58:31 +0800

    Update dns_tomato.js

--- script/dns_tomato.js
+++ script/dns_tomato.js
@@ -1,5 +1,5 @@
 if ($network.wifi.ssid === 'Tomato50') {
-  $done({ address: '192.168.50.50', ttl: 6000 })
+  $done({ address: '10.50.1.50', ttl: 6000 })
 } else {
   $done({});  // Fallback to standard DND query
 }
